51e45114bf55a80e966edd3a6d75a542182d17a2
zyinmatrix <zy902@nyu.edu>
2023-01-16 02:28:18 -0500

    Revert "Revert "Updated the magnitude bounds for response curve""
    This reverts commit 384610ed9792664679984c6637137fa40586958c.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -196,7 +196,10 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     const double outputMax = responseArea.getY();
     auto map = [outputMin, outputMax](double input)
     {
-        return juce::jmap(input, -24.0, 24.0, outputMin, outputMax);
+        auto magnitude = input;
+        if (magnitude > 24) magnitude = 24;
+        else if (magnitude < -24) magnitude = -24;
+        return juce::jmap(magnitude, -24.0, 24.0, outputMin, outputMax);
     };
     
     responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
